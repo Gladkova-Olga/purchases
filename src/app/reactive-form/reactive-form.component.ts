8d4b5759b01f0251e9d75466803b95a09b8b07e4
Lisla Fox <lisla@Lislas-MBP.lan>
2023-05-09 19:17:05 +0300

    short learning tasks - reactive forms

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -8,16 +8,40 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveFormComponent {
   myForm: FormGroup;
-  constructor(){
-    this.myForm= new FormGroup({
-      "userName": new FormControl("Ann", Validators.required),
-      "userEmail": new FormControl("", [Validators.required, Validators.email]),
-      "userPhone": new FormControl("", Validators.pattern("[0-9]{10}"))
-    });
+  // constructor(){
+  //   this.myForm= new FormGroup({
+  //     "userName": new FormControl("Ann", [Validators.required, this.userNameValidator]),
+  //     "userEmail": new FormControl("", [Validators.required, Validators.email]),
+  //     // "userPhone": new FormControl("", Validators.pattern("[0-9]{10}"))
+  //     "phones": new FormArray([
+  //       new FormControl("+370", Validators.required)
+  //     ])
+  //   });
 
-  }; 
+  // }; 
+
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = formBuilder.group({
+      "userName": ["Ann", [Validators.required]],
+      "userEmail": ["", [Validators.required, Validators.email]],
+      "phones": formBuilder.array([
+       [ "+370", Validators.required]])
+    })
+  }
   submit(){
       console.log(this.myForm)
+  } 
+  getFormControls(): FormArray{
+    return this.myForm.controls['phones'] as FormArray;
+  }
+  addPhone() {
+    (<FormArray>this.myForm.controls['phones']).push(new FormControl("+370", Validators.required))
+  }
+  userNameValidator(control: FormControl): {[s:string] :boolean} | null{
+    if(control.value === "no") {
+      return{'userName': true}
+    }
+    return null;
   }
   
 
