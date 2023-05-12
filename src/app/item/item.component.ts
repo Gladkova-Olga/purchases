023f6fa0e65986998c4e66540272a96d3b3a8c73
Lisla Fox <lisla@Lislas-MBP.lan>
2023-05-12 15:54:09 +0300

    short learning tasks - Routing params

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Routes } from '@angular/router';
 import { Subscription, switchMap } from 'rxjs';
 
 @Component({
@@ -8,20 +8,37 @@ import { Subscription, switchMap } from 'rxjs';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit{
-  id: number | undefined;
+  // id: number | undefined;
   // private subscription: Subscription;
-  constructor(private activateRoute: ActivatedRoute) {
+  // constructor(private activateRoute: ActivatedRoute) {
     // this.id = activateRoute.snapshot.params['id'];
     // this.subscription = activateRoute.params.subscribe(params => this.id=params['id']);
 
    
-  }
+  // }
 
  // OR
   ngOnInit() {
-    this.activateRoute.paramMap.pipe(
-      switchMap(params => params.getAll('id'))
-    )
-    .subscribe(data => this.id = +data)
+    // this.activateRoute.paramMap.pipe(
+    //   switchMap(params => params.getAll('id'))
+    // )
+    // .subscribe(data => this.id = +data)
   }
+id: any;
+product: any;
+price: any;
+
+private routeSubscriptoin: Subscription;
+private querySubscription: Subscription;
+constructor(private route: ActivatedRoute) {
+  this.routeSubscriptoin = route.params.subscribe(params =>this.id=params['id']);
+  this.querySubscription = route.queryParams.subscribe(
+    (queryParams: any) => {
+      this.product = queryParams['product'];
+      this.price = queryParams['price']
+    }
+  )
+}
+
+
 }
